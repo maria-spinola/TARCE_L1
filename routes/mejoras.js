3a8db2a5b99edc25c1c4de6b59749b64a9945b95
Maria <maria.spinola.lasso@gmail.com>
2022-11-21 21:06:42 +0100

    vTARCE1

--- routes/mejoras.js
+++ routes/mejoras.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('mejoras', {title:'Mejoras'});
+  res.render('mejoras');
 });
 
 module.exports = router;
