d6366fc8b85569849ebe0c0a24e5e63cc042fcea
Maria <maria.spinola.lasso@gmail.com>
2022-11-23 18:28:21 +0100

    vTARCE2

--- routes/bd.js
+++ routes/bd.js
@@ -1,9 +1,27 @@
+
+const {Pool} = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('bd');
-});
+router.get('/', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('bd', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error" + err);
+  }
+})
 
 module.exports = router;
