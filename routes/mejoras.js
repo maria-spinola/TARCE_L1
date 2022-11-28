cd147d04eb3a28312583e643c7257a0e9ac52325
Maria <maria.spinola.lasso@gmail.com>
2022-11-28 21:47:25 +0100

    v5TARCE

--- routes/mejoras.js
+++ routes/mejoras.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var cool = require('cool-ascii-faces')
+
+router.get('/mejoras', function(request,response){
+  response.send(cool());
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
